6961901f3cb721a0a82331916115ee47e7468743
hantao05 <hantao05@baidu.com>
2017-10-31 17:15:46 +0800

    数据类型与变量

---
+++ 第一章、python基础/data_type.py
@@ -0,0 +1,18 @@
+# 数值类型
+a = 12;
+print(a);
+b = 3.14;
+print(b);
+print(a+b);
+s = "hello world";
+print(s);
+s2 = r'///t///';
+print(s2);
+s3 = '''
+	hello
+	world''';
+print(s3);
+print(True and True);
+print(True or False);
+print(False and False);
+print(None);
