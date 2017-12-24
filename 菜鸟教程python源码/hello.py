8101e4024203a54ab7eb8faef14c8b46527e9cdf
hantao <newbieto@163.com>
2017-12-24 16:17:18 +0800

    注释和缩进

--- 菜鸟教程python源码/hello.py
+++ 菜鸟教程python源码/hello.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+# python的单行注释以“#” 开头
 print("hello world!");
+'''
+	python的多行注释
+	使用三个单引号括起来
+	或者使用三个双引号括起来
+'''
+# python 的代码块是以缩进控制的
+if True:
+	print("true");
+else:
+	print("false");
+
