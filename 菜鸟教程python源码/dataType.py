890a0d399fcc08eb75ffa0deefa813e89060e930
newbietao <newbietao@163.com>
2018-03-10 11:01:57 +0800

    字符串的拼接和字符串的截取和重复

--- 菜鸟教程python源码/dataType.py
+++ 菜鸟教程python源码/dataType.py
@@ -5,15 +5,41 @@
 变量声明时不需要指定数据类型，赋值什么类型的值，变量就是什么类型的
 
 '''
+
 # 变量赋值
-test = 123;
-print(type(test));
-test = 'hello world';
-print(type(test));
+TEST = 123;
+print(type(TEST));
+TEST = 'hello world';
+print(type(TEST));
 # 多个变量赋值
-a = b = c = 12;
-print(a,b,c);
+a = b = c = 124;
+print(a,b,c);3
 a,b,c = 12,22,33;
 print(a,b,c);
 
-# 数据类型
+# 数据类型type函数
+strs = "hello world"
+print(type(str))
+num = 1000
+print(type(num))
+boolean = True
+print(type(boolean))
+
+# instance 函数
+print(isinstance(num, int))
+print(isinstance(strs, str))
+# del关键字，删除变量
+print(num)
+del num
+# print(num)
+# string
+print(strs)
+print(strs[0:5])
+print(strs[6:12])
+print(strs[0])
+# 字符串拼接
+print("hello" + "world")
+# 字符串重复
+print("hello world" * 3)
+
+
